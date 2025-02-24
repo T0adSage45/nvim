a60d8b4e198007fe7ab5c2a566cdf5e120391b61
T0adSage45 <okami8542@gmail.com>
2025-02-24 15:48:58 +0900

    added dashboard

--- lua/ToadSage/lazy/neotree.lua
+++ lua/ToadSage/lazy/neotree.lua
@@ -1,14 +1,13 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window
-	},
-	config = function()
-		-- Set keymap for toggling Neo-Tree
-		vim.keymap.set("n", "<C-b>", ":Neotree toggle reveal filesystem right show <CR>", {})
-	end,
-} --- use <C-b> then ?.
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
+}
